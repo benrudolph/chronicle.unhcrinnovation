3e52deaa6092308ee1ec995011ada1b3a1e0ab22
Ben Rudolph <rudolphben@gmail.com>
2014-06-10 21:46:48 +0200

    Basic working example

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
-
+var nano = require('nano');
+var dbWords = nano('http://localhost:5984/words');
+var dbSpeeches = nano('http://localhost:5984/speeches');
+var natural = require('natural');
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'UNHCR Diction' });
+  dbSpeeches.view('application', 'by_id', function(err, body) {
+    res.render('index', { data: JSON.stringify(body) });
+  });
+};
+
+exports.search = function(req, res) {
+  var query = req.param('query');
+
+  dbWords.view('application', 'by_word', { key: natural.PorterStemmer.stem(query) }, function(err, body) {
+    res.json(200, body);
+  });
+
 };
