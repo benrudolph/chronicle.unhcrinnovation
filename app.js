c333cf242db280d6a7854f0640ec3b484612a29e
Ben Rudolph <rudolphben@gmail.com>
2014-06-15 18:50:54 +0200

    Do not sync db auto

--- app.js
+++ app.js
@@ -31,16 +31,8 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/search', routes.search);
 app.get('/word_counts', routes.wordCounts);
+app.get('/sentences', routes.sentences);
 
-db
-  .sequelize
-  .sync({ force: true })
-  .complete(function(err) {
-    if (err) {
-      throw err[0];
-    } else {
-      http.createServer(app).listen(app.get('port'), function(){
-        console.log('Express server listening on port ' + app.get('port'));
-      });
-    }
-  });
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
