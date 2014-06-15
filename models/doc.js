d46562be51575029f1f355e63be7c2ae5db1ba71
Ben Rudolph <rudolphben@gmail.com>
2014-06-15 18:52:25 +0200

    publicModels and wordCount column

--- models/doc.js
+++ models/doc.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 module.exports = function(sequelize, DataTypes) {
   var Doc = sequelize.define('Doc', {
     documentId: DataTypes.STRING,
@@ -7,6 +9,7 @@ module.exports = function(sequelize, DataTypes) {
     by: DataTypes.STRING,
     sentiment_comp: DataTypes.INTEGER,
     sentiment_score: DataTypes.INTEGER,
+    wordCount: DataTypes.INTEGER,
     content: DataTypes.TEXT
 
   }, {
@@ -14,8 +17,22 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         Doc.hasMany(models.WordDoc);
         Doc.hasMany(models.SentenceDoc);
+      },
+      publicModels: function(models) {
+        return _.map(models, function(model) {
+          return {
+            id: model.id,
+            documentId: model.documentId,
+            sentiment_comp: model.sentiment_comp,
+            sentiment_score: model.sentiment_score,
+            author: model.author,
+            date: model.date,
+            title: model.title
+          };
+        });
+
       }
-    }
+    },
   });
 
   return Doc;
